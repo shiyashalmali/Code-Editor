5dc1ba16802910d9df36a1b89fa2e72d9a81d5c4
Shiya Shalmali Shetty <shalmalishiya@gmail.com>
2024-07-26 23:48:15 +0530

    new

---
+++ editor/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+import { AppBar, Toolbar, styled } from "@mui/material"
+
+const Container = styled(AppBar)`
+    background: #060606;
+    position: static;
+    height: 2vh;
+`;
+
+const Header = () => {
+    
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Header;
